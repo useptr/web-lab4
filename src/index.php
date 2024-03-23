a9e88ad9ead5f3c6e1152bdf0bf5d16aef429e88
useptr <wollow.gm@gmail.com>
2024-03-23 15:42:11 +0700

    create repo

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+// $servername = "mysql";
+// $username = "root";
+// $password = "root";
+// $connection->query('CREATE DATABASE `lemp_docker`');
+// $connection = mysqli_connect($servername, $username, $password);
+
+$dbhost = "mysql";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "app";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
+
+// $sql = "INSERT INTO users (login, password) VALUES ('example_login', 'example_password')";
+// $conn->query($sql);
+$sql = "SELECT * FROM `users`";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "login: " . $row["login"]. ", password: " . $row["password"]. "<br>";
+    }
+  }
+
+// echo $result;
+// var_dump($result);
+// echo "Hello";
+?>
